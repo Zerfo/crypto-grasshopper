8b6cfa5d73566b09f7d3d77c051f210fbd475778
sergeishmakov <m.shmakov.sergei@yandex.ru>
2021-12-16 22:55:00 +0300

    improvements

--- util/index.js
+++ util/index.js
@@ -1,29 +1,4 @@
-import _ from "lodash";
-
-// export function GOST_Kuz_Encrypt(blk) {
-//   let outBlk = blk;
-
-//   for (let i = 0; i < 9; i++) {
-//     outBlk = GOST_Kuz_X(iterKey[i], outBlk);
-//     outBlk = GOST_Kuz_S(outBlk);
-//     outBlk = methodL(outBlk);
-//   }
-//   outBlk = GOST_Kuz_X(outBlk, iterKey[9]);
-
-//   return outBlk;
-// }
-
-// export function GOST_Kuz_Decrypt(blk) {
-//   let outBlk = GOST_Kuz_X(blk, iterKey[9]);
-
-//   for (let i = 8; i >= 0; i--) {
-//     outBlk = GOST_Kuz_reverse_L(outBlk);
-//     outBlk = GOST_Kuz_reverse_S(outBlk);
-//     outBlk = GOST_Kuz_X(iterKey[i], outBlk);
-//   }
-
-//   return outBlk;
-// }
+import { chunk } from "lodash";
 
 export function hexToBytes(hex) {
   for (var bytes = [], c = 0; c < hex.length; c += 2)
@@ -37,7 +12,7 @@ export function bytesToHex(bytes) {
     hex.push((current >>> 4).toString(16));
     hex.push((current & 0xf).toString(16));
   }
-  return _.chunk(hex, 16)
+  return chunk(hex, 16)
     .map((l) => l.reverse())
     .reverse()
     .flat()
